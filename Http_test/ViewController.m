edb172c98e52d93686d21fdd05604884fc7056fd
鹏刘 <pengliu@pengdeMacBook-Pro.local>
2017-04-13 07:15:52 +0800

    aides

--- Http_test/ViewController.m
+++ Http_test/ViewController.m
@@ -8,22 +8,46 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
+@interface ViewController ()
+@property (nonatomic,strong) UIImageView *imageView;
+@property (nonatomic) NSData *imageData;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self initImageView];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)initImageView
+{
+    self.imageView = [[UIImageView alloc] init];
+    self.imageView.frame = CGRectMake(20, 55, 320, 350);
+    self.imageView.backgroundColor = [UIColor greenColor];
+    
+   self.imageView.image = [UIImage imageWithData:self.imageData];
+    [self.view addSubview:self.imageView];
 }
+
+- (void)downLoadImage
+{
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURL *imageURL = [NSURL URLWithString:@"http://localhost:8080/MJServer/resources/images/minion_01.png"];
+    
+    NSURLSessionTask *task = [session downloadTaskWithURL:imageURL completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSString *imagePath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:response.suggestedFilename];
+        
+        NSFileManager *file = [NSFileManager defaultManager];
+        [file moveItemAtURL:location toURL:[NSURL URLWithString:imagePath] error:nil];
+        self.imageData = [file contentsAtPath:imagePath];
+    }];
+
+    [task resume];
+}
+
 
 
 @end
